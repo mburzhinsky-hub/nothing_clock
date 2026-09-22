ab8d12d071beb29f03a372b26fa968ac7bf25221
mburzhinsky-hub <m.burzhinsky@visuals.ru>
2026-09-22 11:31:19 +0300

    Refresh PWA cache for animated clock

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "glyph-clock-v1";
+const CACHE = "glyph-clock-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -24,14 +24,14 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then((cached) =>
-      cached ||
-      fetch(event.request).then((response) => {
+    fetch(event.request)
+      .then((response) => {
         const copy = response.clone();
         caches.open(CACHE).then((cache) => cache.put(event.request, copy));
         return response;
       })
-    )
+      .catch(() => caches.match(event.request))
   );
 });
